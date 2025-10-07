1d4144dc3401f65648818b8ea314b692022b19a9
paulo-esco <116454139+paulo-esco@users.noreply.github.com>
2025-10-07 05:43:51 +0000

    Add project body

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+thread_ids = list(map(int, os.getenv("THREAD_IDS").split(",")))
+token = os.getenv("TOKEN")
+
+
+
+url = "https://prod-api.lolz.live/threads/@url@/bump"
+
+headers = {
+    "accept": "application/json",
+    "authorization": f"Bearer {token}"
+}
+
+
+def main():
+    for thread_id in thread_ids:
+        response = requests.post(url.replace("@url@", str(thread_id)), headers=headers)
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
+
